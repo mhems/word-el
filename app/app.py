365055a6b11d9cd64d89d20825c344e449db3d00
matthew <matthems@gmail.com>
2024-07-16 21:03:39 -0400

    start using jinja templates and play with keyboard coloring

--- app/app.py
+++ app/app.py
@@ -1,11 +1,14 @@
 from flask import (
     Flask,
     render_template,
-    request
+    request,
+    g
 )
 from db import (
     init_app        
 )
+
+from itertools import takewhile
 
 app = Flask('word-el')
 init_app(app)
@@ -14,10 +17,19 @@ init_app(app)
 def index():
     return render_template('index.html')
 
+def get_last_word(inputs: {str: str}):
+    rows = [(row, ''.join(inputs['r%d-%d' % (row, col)].upper() for col in range(5))) for row in range(5)]
+    return list(takewhile(lambda x: len(x[1]) == 5, rows))[-1]
+
 @app.route('/game', methods=['GET', 'POST'])
 def game():
     if request.method == 'POST':
-        app.logger.info(request.form)
+        row, word = get_last_word(request.form)
+        app.logger.debug(str(row) + " " + word)
+        g.keys = dict()
+        g.keys['a'] = 'present'
+        g.keys['z'] = 'absent'
+        g.keys['l'] = 'correct'
     return render_template('game.html')
 
 @app.route('/user/<username>')
